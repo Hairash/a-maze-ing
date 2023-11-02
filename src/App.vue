18f7e7745ba34737464e764cec4ad69cc50db8c3
Hairash <maslevian@yandex.ru>
2023-11-02 21:26:19 +0300

    Add touch handlers
    * Small refactoring

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,22 @@
+<template>
+  <main>
+    <Board
+      :cell-size=cellSize
+      :width=width
+      :height=height
+      :field=field
+      :hero-x=heroX
+      :hero-y=heroY
+      :hero-sight=heroSight
+    />
+  </main>
+</template>
+
+
 <script>
 import Board from './components/Board.vue'
 import { processKey, scrollToPoint, initLevel } from './game/engine.js'
 import * as consts from './game/const.js'
-
 
 export default {
   name: 'App',
@@ -23,6 +37,7 @@ export default {
   },
   created() {
     initLevel(this);
+
     window.addEventListener('load', (e) => {
       // TODO: Forbid manual scroll and hide scrollers
       scrollToPoint(this.heroX, this.heroY, this.cellSize);
@@ -33,23 +48,40 @@ export default {
       e.preventDefault();
       processKey(e.key, this);
     });
+
+    window.addEventListener('touchstart', (e) => {
+      // e.preventDefault();
+      const touch = e.touches[0];
+      // console.log(touch);
+      const touchX = touch.screenX;
+      // console.log(touchX);
+      const touchY = touch.screenY;
+      // console.log(touchY);
+
+      // Get the dimensions of the screen / container
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      // console.log(width, height);
+
+      // Determine which part of the screen was touched
+      if (touchX > width * 0.7) {
+        // Right part of the screen
+        processKey('ArrowRight', this);
+      } else if (touchX < width * 0.3) {
+        // Left part of the screen
+        processKey('ArrowLeft', this);
+      } else if (touchY > height * 0.7) {
+        // Bottom part of the screen
+        processKey('ArrowDown', this);
+      } else if (touchY < height * 0.3) {
+        // Top part of the screen
+        processKey('ArrowUp', this);
+      }
+    });
   }
 }
 </script>
 
-<template>
-  <main>
-    <Board
-      :cell-size=cellSize
-      :width=width
-      :height=height
-      :field=field
-      :hero-x=heroX
-      :hero-y=heroY
-      :hero-sight=heroSight
-    />
-  </main>
-</template>
 
 <style>
 @import './assets/base.css';
